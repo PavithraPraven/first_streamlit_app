4823fcc88d5d1000e62e474b0154951986ca0a9e
PavithraPraven <139545824+PavithraPraven@users.noreply.github.com>
2023-09-05 20:48:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('My Mom's new healthy dinner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Blueberry meal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avacoda Toast')
+streamlit.text(' 🥣 Omega 3 & Blueberry meal')
+streamlit.text(' 🥗 Kale, Spinach & Rocket Smothie')
+streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
+streamlit.text(' 🥑🍞 Avacoda Toast')
